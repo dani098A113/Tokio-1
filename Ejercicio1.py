b6cbf091b8cca6224c84c0b3767382a1414b3993
Daniel-098 <josedaniel3938@gmail.com>
2024-06-07 17:16:10 -0600

    Actualización de archivos nuevos

---
+++ Ejercicio1.py
@@ -0,0 +1,13 @@
+n1=7
+n2=1
+n3=4
+n4=2
+n5=2
+sum=n1+n2+n3+n4+n5
+res=n1-n2-n3-n4-n5
+div=n1/n2/n3/n4/n5
+mul=n1*n2*n3*n4*n5
+print(("La suma de los numero es de : ",sum))
+print(("La resta de los numero es de : ",res))
+print(("La divición de los numero es de : ",div))
+print(("La multiplicación de los numero es de : ",mul))
